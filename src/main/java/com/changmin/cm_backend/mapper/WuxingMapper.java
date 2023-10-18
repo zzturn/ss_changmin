4f174cf13acf271aca18bd35971307bc32c5cf76
zzturn <luoxin9712@163.com>
2023-10-18 17:40:36 +0800

    [feat]通过id获取wuxing时，可以查询到已删除的do

--- src/main/java/com/changmin/cm_backend/mapper/WuxingMapper.java
+++ src/main/java/com/changmin/cm_backend/mapper/WuxingMapper.java
@@ -2,6 +2,10 @@ package com.changmin.cm_backend.mapper;
 
 import com.changmin.cm_backend.config.mybatis.core.mapper.BaseMapperX;
 import com.changmin.cm_backend.model.pojo.*;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
-public interface WuxingMapper extends BaseMapperX<WuxingDO> {}
-
+@Repository
+public interface WuxingMapper extends BaseMapperX<WuxingDO> {
+  WuxingDO selectWuxingById(@Param("id") String id);
+}
